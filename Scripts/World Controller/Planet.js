1f74e1e46770c8db8b705845fa4dd919e4fba2bd
artiespeedy <alexandertmai@gmail.com>
2014-05-07 20:21:08 -0700

    Created Directories

---
+++ Scripts/World Controller/Planet.js
@@ -0,0 +1,14 @@
+var NEWTONS_PER_UNIT : int = 9.55;
+
+public class Planet(size, color, type, position) {
+  this.size = size;
+  this.color = color;
+  this.type = type;
+  this.position = position;
+
+  this.gravity = size * 9.55;
+
+  function getForceVector(otherPosition) {
+    var
+  }
+}
