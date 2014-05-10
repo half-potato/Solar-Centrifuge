4a19465f93506c88245a99db934a53ec0a6763cd
artiespeedy <alexandertmai@gmail.com>
2014-05-10 10:01:21 -0700

    Stuff

---
+++ Assets/Scripts/World Controller/Planet.js
@@ -0,0 +1,18 @@
+var NEWTONS_PER_UNIT : int = 9.55;
+
+public class Planet(size, color, type, position) {
+  this.size = size;
+  this.color = color;
+  this.type = type;
+  this.position = position;
+
+  this.gravity = size * 9.55;
+
+  function getForceVector(otherPosition) {
+    var outputVector = new Vector3(0, 0, 0);
+    outputVector.x = position.x - otherPosition.x;
+    outputVector.y = position.y - otherPosition.y;
+    outputVector.setLength(outputVector.length / gravity);
+    return outputVector;
+  }
+}
